4045b228e7c19fa10e102f4df4b348e533705d41
hib4 <hiba.kudus@gmail.com>
2023-03-23 08:49:08 +0700

    feat: [HF] domain layer refactoring

---
+++ lib/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_clean_architecture/core/error/failures.dart';
+import 'package:flutter_clean_architecture/core/use_cases/use_case.dart';
+import 'package:flutter_clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:flutter_clean_architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
+  GetConcreteNumberTrivia(this.repository);
+
+  final NumberTriviaRepository repository;
+
+  @override
+  Future<Either<Failure, NumberTrivia>> call(Params params) async {
+    return await repository.getConcreteNumberTrivia(params.number);
+  }
+}
+ class Params extends Equatable {
+  const Params({required this.number});
+
+  final int number;
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [number];
+ }
--- lib/features/number_trivia/domain/use_cases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/use_cases/get_random_number_trivia.dart
@@ -1,14 +1,17 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_clean_architecture/core/error/failures.dart';
+import 'package:flutter_clean_architecture/core/use_cases/use_case.dart';
 import 'package:flutter_clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_clean_architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
-class GetConcreteNumberTrivia {
-  GetConcreteNumberTrivia(this.repository);
+class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
+  GetRandomNumberTrivia(this.repository);
 
   final NumberTriviaRepository repository;
 
-  Future<Either<Failure, NumberTrivia>> execute({required int number}) async {
-    return await repository.getConcreteNumberTrivia(number);
+  @override
+  Future<Either<Failure, NumberTrivia>> call(NoParams params) async {
+    // TODO: implement call
+    return await repository.getRandomNumberTrivia();
   }
 }
